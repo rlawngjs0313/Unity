781bff03846e0b86c81986363fee52b41b013def
IlIllIlIIlll <juheon0313@naver.com>
2021-10-26 10:31:55 +0900

    우리 진영, 적 진영에 낮은 속도로 돌아다니는 봇 구현 #1

---
+++ Project/Assets/Scripts/Obstacle_AI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Obstacle_AI : MonoBehaviour
+{
+    private void Start() 
+    {
+        
+    }
+    private void Update() 
+    {
+        
+    }
+}
