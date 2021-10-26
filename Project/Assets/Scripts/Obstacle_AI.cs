83ec0bcf5a3912e7c9be0b206dfa9840f5a7fca3
IlIllIlIIlll <juheon0313@naver.com>
2021-10-26 12:40:02 +0900

    우리 진영, 적 진영에 돌아다니는 봇 구현 #2

--- Project/Assets/Scripts/Obstacle_AI.cs
+++ Project/Assets/Scripts/Obstacle_AI.cs
@@ -3,12 +3,31 @@ using UnityEngine.AI;
 
 public class Obstacle_AI : MonoBehaviour
 {
+    Core core;
+    NavMeshAgent nav;
+    int test = 0;
     private void Start() 
     {
-        
+        core = GameObject.Find("GameSystem").GetComponent<Core>();
+        nav = GetComponent<NavMeshAgent>();
+        test = Random.Range(-100, 100);
+        transform.position = new Vector3(test, 1, 0);
     }
     private void Update() 
     {
-        
+        move();
+    }
+    void move()
+    {
+        if(test >= 0)
+            Debug.Log(test);
+        if(transform.position.x > -12)            
+        {
+            if(transform.position.x < 12)
+            {
+                Object.Destroy(this);
+                transform.position = new Vector3(Random.Range(-100, 100), 1, 0);
+            }
+        }
     }
 }
